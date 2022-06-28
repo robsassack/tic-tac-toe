e1764c118fc052318b800c21e97a71fdc01bf4b1
Rob Sassack <rsassack25@gmail.com>
2022-06-28 15:27:13 -0400

    Algorithm added to figure out winner

--- script.js
+++ script.js
@@ -40,22 +40,53 @@ const displayController = (() => {
           gameBoard.board[x][y] = "O";
           turn = !turn;
         }
-        gameOver();
+        let winner = checkWinner(gameBoard.board);
+        if (winner) {
+          console.log(winner);
+        }
       });
     }
   }
 })();
 
-function gameOver() {
-  let isFull = true;
+function checkWinner(board) {
+  for (i=0; i<3; i++) {
+    // check for vertical lines
+    if ((board[i][0] !== '' &&
+      board[i][0] === board[i][1] &&
+      board[i][1] === board[i][2])) {
+      return board[i][0];
+    // check for horizontal lines
+    } else if ((board[0][i] !== '' &&
+      board[0][i] === board[1][i] &&
+      board[1][i] === board[2][i])) {
+      return board[0][i];
+    }
+  }
+  // check for diagonals
+  if (
+    board[0][0] !== '' &&
+    board[0][0] === board[1][1] &&
+    board[1][1] == board[2][2]) {
+      return board[1][1];
+  } else if (
+    board[2][0] !== '' &&
+    board[2][0] === board[1][1] &&
+    board[1][1] === board[0][2]){
+      return board[1][1];
+  }
+  if (emptyBoard() === true) {
+    return -1;
+  }
+}
+
+function emptyBoard() {
   for (let x = 0; x < gameBoard.elements.length; x++) {
     for (let y = 0; y < gameBoard.elements[x].length; y++) {
       if (gameBoard.board[x][y] === '') {
-        isFull = false;
+        return false; // empty square on board found
       }
     }
   }
-  if (isFull === true) {
-    // game board is full
-  }
+  return true; // none of the squares on the board are empty
 }
