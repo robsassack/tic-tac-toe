731558e3b033c785c60fedd6fbd7035d2f733ca2
Rob Sassack <rsassack25@gmail.com>
2022-07-01 16:16:50 -0400

    Updated formatting

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ const displayController = (() => {
 })();
 
 function buttonListeners() {
-  const grid = document.querySelectorAll('.game-board-square');
+  const grid = document.querySelectorAll(".game-board-square");
   grid.forEach((current) => {
-    current.addEventListener('click', logConsole);
+    current.addEventListener("click", logConsole);
   });
 
   let resetButton = document.querySelector(".new-game");
@@ -70,7 +70,7 @@ function resetBoard() {
   grid.forEach((element) => {
     element.classList.remove("select-X");
     element.classList.remove("select-O");
-  })
+  });
   gameBoard.turn = true;
 }
 
@@ -95,36 +95,42 @@ function disableBoard() {
   for (let x = 0; x < gameBoard.elements.length; x++) {
     for (let y = 0; y < gameBoard.elements[x].length; y++) {
       let current = gameBoard.elements[x][y];
-      current.removeEventListener('click', logConsole);
+      current.removeEventListener("click", logConsole);
     }
   }
 }
 
 function checkWinner(board) {
-  for (i=0; i<3; i++) {
+  for (i = 0; i < 3; i++) {
     // check for vertical lines
-    if ((board[i][0] !== '' &&
+    if (
+      board[i][0] !== "" &&
       board[i][0] === board[i][1] &&
-      board[i][1] === board[i][2])) {
+      board[i][1] === board[i][2]
+    ) {
       return board[i][0];
-    // check for horizontal lines
-    } else if ((board[0][i] !== '' &&
+      // check for horizontal lines
+    } else if (
+      board[0][i] !== "" &&
       board[0][i] === board[1][i] &&
-      board[1][i] === board[2][i])) {
+      board[1][i] === board[2][i]
+    ) {
       return board[0][i];
     }
   }
   // check for diagonals
   if (
-    board[0][0] !== '' &&
+    board[0][0] !== "" &&
     board[0][0] === board[1][1] &&
-    board[1][1] == board[2][2]) {
-      return board[1][1];
+    board[1][1] == board[2][2]
+  ) {
+    return board[1][1];
   } else if (
-    board[2][0] !== '' &&
+    board[2][0] !== "" &&
     board[2][0] === board[1][1] &&
-    board[1][1] === board[0][2]){
-      return board[1][1];
+    board[1][1] === board[0][2]
+  ) {
+    return board[1][1];
   }
   if (isBoardEmpty() === true) {
     return -1; // returned if no winner
@@ -134,7 +140,7 @@ function checkWinner(board) {
 function isBoardEmpty() {
   for (let x = 0; x < gameBoard.elements.length; x++) {
     for (let y = 0; y < gameBoard.elements[x].length; y++) {
-      if (gameBoard.board[x][y] === '') {
+      if (gameBoard.board[x][y] === "") {
         return false; // empty square on board found
       }
     }
